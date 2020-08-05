9013d80c3e9b386cf08ffd59a9986600c7a284cc
EikE SousA <eike.sousa@hotmail.com>
2020-08-05 00:25:17 -0300

    aula01

---
+++ web/src/pages/TeacherForm/index.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+import { PageHeader } from "../../components";
+
+import { Container } from "./styles";
+
+const TeacherForm: React.FC = () => {
+  return (
+    <Container id="list">
+      <PageHeader title="Que incrível que você quer dar aulas." />
+    </Container>
+  );
+};
+
+export default TeacherForm;
---
+++ web/src/pages/TeacherList/index.tsx
@@ -0,0 +1,36 @@
+import React from "react";
+
+import { PageHeader, TeacherItem } from "../../components";
+
+import { Container } from "./styles";
+
+const TeacherList: React.FC = () => {
+  return (
+    <Container id="list">
+      <PageHeader title="Esses são os proffys disponíveis.">
+        <form id="seacrh-teachers">
+          <div className="input-block">
+            <label htmlFor="subject">Matéria</label>
+            <input type="text" id="subject" />
+          </div>
+
+          <div className="input-block">
+            <label htmlFor="week_day">Dia da Semana</label>
+            <input type="text" id="week_day" />
+          </div>
+
+          <div className="input-block">
+            <label htmlFor="time">Hora</label>
+            <input type="text" id="time" />
+          </div>
+        </form>
+      </PageHeader>
+
+      <main>
+        <TeacherItem />
+      </main>
+    </Container>
+  );
+};
+
+export default TeacherList;
